d3342c19ca0f107f31af5c550c235c2c84bbce3c
Rajeev Singh <rajeevsinghup30@gmail.com>
2022-05-20 10:52:49 +0530

    Added Target Date for Todo - Used initBinder to Handle Date Fields

--- src/main/java/com/in28minutes/springboot/web/controller/TodoController.java
+++ src/main/java/com/in28minutes/springboot/web/controller/TodoController.java
@@ -1,13 +1,17 @@
 package com.in28minutes.springboot.web.controller;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,7 +25,15 @@ public class TodoController {
 
 	@Autowired
 	TodoService service;
+	
+	@InitBinder
+	protected void initBinder(WebDataBinder binder) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(
+				dateFormat, false));
+	}
 
+	
 	@RequestMapping(value = "/list-todos", method = RequestMethod.GET)
 	public String showTodos(ModelMap model) {
 		String name = (String) model.get("name");
@@ -49,7 +61,7 @@ public class TodoController {
 			return "todo";
 		}
 		
-		service.addTodo((String) model.get("name"), todo.getDesc(), new Date(),
+		service.addTodo((String) model.get("name"), todo.getDesc(), todo.getTargetDate(),
 				false);
 		return "redirect:/list-todos";
 	}
@@ -64,13 +76,16 @@ public class TodoController {
 	@RequestMapping(value = "/update-todo", method = RequestMethod.POST)
 	public String UpdateTodo( ModelMap model, @Valid Todo todo, BindingResult result) {
 	  
-		todo.setUser((String) model.get("name"));
-		if(result.hasErrors()){
+			if(result.hasErrors()){
 			return "todo";
 		}
+			
+		todo.setUser((String) model.get("name"));
 		
-		service.addTodo((String) model.get("name"), todo.getDesc(), new Date(),
-				false);
+		service.updateTodo(todo);
+		
+	//	service.addTodo((String) model.get("name"), todo.getDesc(), new Date(),
+	//			false);
 		return "redirect:/list-todos";
 	
 	}
