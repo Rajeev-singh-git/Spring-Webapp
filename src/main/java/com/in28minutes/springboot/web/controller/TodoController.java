8004cb948c1d8bad37c35fe43d9c18b3498299a0
Rajeev Singh <rajeevsinghup30@gmail.com>
2022-05-22 11:12:00 +0530

    - Prepare for Using Spring Security - Remove All the Login Related Functionality - Make Welcome the default page - with some hardcoding to start with. - Refactor getLoggedInUserName - Update Home Page Link in navigation

--- src/main/java/com/in28minutes/springboot/web/controller/TodoController.java
+++ src/main/java/com/in28minutes/springboot/web/controller/TodoController.java
@@ -36,14 +36,19 @@ public class TodoController {
 	
 	@RequestMapping(value = "/list-todos", method = RequestMethod.GET)
 	public String showTodos(ModelMap model) {
-		String name = (String) model.get("name");
+		String name = getLoggedInUserName(model);
 		model.put("todos", service.retrieveTodos(name));
 		return "list-todos";
 	}
 
+
+	private String getLoggedInUserName(ModelMap model) {
+		return (String) model.get("name");
+	}
+
 	@RequestMapping(value = "/add-todo", method = RequestMethod.GET)
 	public String showAddTodoPage(ModelMap model) {
-		model.addAttribute("todo", new Todo(0, (String) model.get("name"), "Default Desc",
+		model.addAttribute("todo", new Todo(0, getLoggedInUserName(model), "Default Desc",
 				new Date(), false));
 		return "todo";
 	}
@@ -61,7 +66,7 @@ public class TodoController {
 			return "todo";
 		}
 		
-		service.addTodo((String) model.get("name"), todo.getDesc(), todo.getTargetDate(),
+		service.addTodo(getLoggedInUserName(model), todo.getDesc(), todo.getTargetDate(),
 				false);
 		return "redirect:/list-todos";
 	}
@@ -80,7 +85,7 @@ public class TodoController {
 			return "todo";
 		}
 			
-		todo.setUser((String) model.get("name"));
+		todo.setUser(getLoggedInUserName(model));
 		
 		service.updateTodo(todo);
 		
